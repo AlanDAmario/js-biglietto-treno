ae1db122e800aa1f2c4e139fa8fe6aab5d86ebb8
AlanDAmario <vega93@live.it>
2024-04-24 15:58:15 +0200

    first commit

---
+++ jss/main.js
@@ -0,0 +1,20 @@
+"use strict";
+// variabile età//
+
+const travelerAge = Number(prompt('inserire l\' eta'));
+console.log(travelerAge);
+
+//variabile km da percorerre//
+
+const travelerKM = Number(prompt('inserire km da percorrere'));
+console.log(travelerKM);
+
+//variabile prezzo per km//
+
+const priceKM = 0.21;
+console.log(priceKM)
+
+//svolgimento//
+
+let total =
+
