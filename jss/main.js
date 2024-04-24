49282fd81673f9e8ce175ebd45c059ca0aaa600c
AlanDAmario <vega93@live.it>
2024-04-24 17:14:23 +0200

    [UPD] end exercise

--- jss/main.js
+++ jss/main.js
@@ -7,14 +7,34 @@ console.log(travelerAge);
 //variabile km da percorerre//
 
 const travelerKM = Number(prompt('inserire km da percorrere'));
-console.log(travelerKM);
+console.log('travelKM', travelerKM);
 
 //variabile prezzo per km//
 
 const priceKM = 0.21;
-console.log(priceKM)
+console.log('priceKM', priceKM);
+
+//PERCENTUALI//
+
+const discount20 = 20;
+
+const discount40 = 40;
 
 //svolgimento//
 
-let total =
+let total = priceKM * travelerKM;
+console.log('total', total);
 
+
+// }
+
+if (travelerAge < 18) {
+    total = (total / 100) * (100 - discount20);
+
+} else if (travelerAge > 65) {
+    total = total * (discount40 / 100);
+
+}
+//ARROTONDAMENTO DECIMALE//
+
+console.log(Math.round(total * 100) / 100);
